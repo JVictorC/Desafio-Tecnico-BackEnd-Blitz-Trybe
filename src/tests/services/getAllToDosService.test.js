a3de236329989c5e75b91500ff64a8fd720c5901
Joao Victor <joaogmavictor@gmail.com>
2022-02-12 16:54:22 -0300

    Test: Implementando Testes para GetAllToDos Service

---
+++ src/tests/services/getAllToDosService.test.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+
+const { getAllToDosService } = require('../../services/toDoServices');
+const todoModels = require('../../models/toDoModels.js');
+const { it, describe, before, after } = require('mocha');
+const Sinon = require('sinon');
+
+describe.only('Busca Todos os ToDos no BD SERVICE', () => {
+
+  describe('quando é buscado com sucesso', async () => {
+    const payloadToDos = [
+      {
+        title: 'Limpar a Casa',
+        description: 'Tenho que Limpar a Casa hoje as 14 da tarde',
+      },
+      {
+        title: 'Fazer Comida',
+        description: 'Tenho que preparar a comida hoje',
+      }
+    ]
+
+    before(() => {
+      Sinon.stub(todoModels, 'getAllToDos').resolves(payloadToDos);
+    });
+
+    after(() => {
+      todoModels.getAllToDos.restore();
+    });
+
+    it('retorna um array de ToDos', async () => {
+      const response = await getAllToDosService();
+      expect(response).to.be.a('array');
+      expect(response).to.deep.equals(payloadToDos);
+    });
+
+  });
+
+});
+
+// ref testes https://github.com/tryber/sd-013-c-live-lectures/tree/lecture/27.4/live-lecture-27-4-base
