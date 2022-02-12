db55601f6085130aa3875429c702d4e1a4710e39
Joao Victor <joaogmavictor@gmail.com>
2022-02-11 22:39:41 -0300

    Test: Implementando Casos de Sucesso Create Service

--- src/tests/services/createNewToDoService.test.js
+++ src/tests/services/createNewToDoService.test.js
@@ -1,7 +1,9 @@
 const { expect } = require('chai');
 
 const { createToDoService } = require('../../services/toDoServices');
-const { it, describe } = require('mocha');
+const todoModels = require('../../models/toDoModels.js');
+const { it, describe, before, after } = require('mocha');
+const Sinon = require('sinon');
 
 describe('Insere um novo ToDo no BD SERVICE', () => {
   describe('quando o payload informado não é válido', async () => {
@@ -22,7 +24,7 @@ describe('Insere um novo ToDo no BD SERVICE', () => {
 
     it('retorna um Error com Status e Message quando nao passado o description', async () => {
       try {
-         await createToDoService({ title: 'Fazer Comida' });
+        await createToDoService({ title: 'Fazer Comida' });
       } catch (error) {
         expect(error).to.be.a('object');
         expect(error).to.deep.equals(messageErrorWithOutDescription);
@@ -30,37 +32,32 @@ describe('Insere um novo ToDo no BD SERVICE', () => {
     });
   });
 
-  // describe('quando é inserido com sucesso', async () => {
-  //   const payloadMovie = {
-  //     title: 'Example Movie',
-  //     directedBy: 'Jane Dow',
-  //     releaseYear: 1999,
-  //   };
+  describe('quando é inserido com sucesso', async () => {
+    const payloadToDo = {
+      title: 'Limpar a Casa',
+      description: 'Tenho que Limpar a Casa hoje as 14 da tarde',
+    };
 
-  //   before(() => {     
-  //     const ID_EXAMPLE = '604cb554311d68f491ba5781';
+    before(() => {
+      const ID_EXAMPLE = '604cb554311d68f491ba5781';
+      Sinon.stub(todoModels, 'createToDo').resolves({ id: ID_EXAMPLE, ...payloadToDo });
+    });
 
-  //     sinon.stub(MoviesModel, 'create')
-  //       .resolves({ id: ID_EXAMPLE });
-  //   });
+    after(() => {
+      todoModels.createToDo.restore();
+    });
 
-  //   after(() => {
-  //     MoviesModel.create.restore();
-  //   });
+    it('retorna um objeto', async () => {
+      const response = await createToDoService(payloadToDo);
+      expect(response).to.be.a('object');
+    });
 
-  //   it('retorna um objeto', async () => {
-  //     const response = await MoviesService.create(payloadMovie);
+    it('tal objeto possui o "id" do novo toDo inserido', async () => {
+      const response = await createToDoService(payloadToDo);
+      expect(response).to.have.a.property('id');
+    });
 
-  //     expect(response).to.be.a('object');
-  //   });
-
-  //   it('tal objeto possui o "id" do novo filme inserido', async () => {
-  //     const response = await MoviesService.create(payloadMovie);
-
-  //     expect(response).to.have.a.property('id');
-  //   });
-
-  // });
+  });
 });
 
 // ref testes https://github.com/tryber/sd-013-c-live-lectures/tree/lecture/27.4/live-lecture-27-4-base
