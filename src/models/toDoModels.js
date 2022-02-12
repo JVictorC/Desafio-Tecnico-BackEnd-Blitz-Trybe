fd4e2df9ae6006b26b4f9bb01c8652684549c8c8
Joao Victor <joaogmavictor@gmail.com>
2022-02-11 21:27:11 -0300

    Build: Implementando Funcionalidde de GetAllToDos

--- src/models/toDoModels.js
+++ src/models/toDoModels.js
@@ -1,5 +1,11 @@
 const conn = require('./connection');
 
+const maskReturnDataBase = (toDo) => ({
+  id: toDo['_id'],
+  title: toDo['title'],
+  description: toDo['description'],
+})
+
 const createToDo = async (newToDo) => {
   const db = await conn.connection();
 
@@ -12,7 +18,16 @@ const createToDo = async (newToDo) => {
 };
 
 
+const getAllToDos = async () => {
+  const db = await conn.connection();
+
+  const allTodos = await db.collection('toDo').find({}).toArray();
+
+  return allTodos.map(maskReturnDataBase);
+}
+
 module.exports = {
-  createToDo
+  createToDo,
+  getAllToDos
 }
 
